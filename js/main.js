f03053ae1f8d5fd34722392a0cab385ca3be5d9a
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-15 13:49:14 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 100; i++) {
+  const isDivisibleBy3 = i % 3 === 0;
+  const isDivisibleBy5 = i % 5 === 0;
+  if (isDivisibleBy3 && isDivisibleBy5) console.log("FizzBuzz");
+  else if (isDivisibleBy3) console.log("Fizz");
+  else if (isDivisibleBy5) console.log("Buzz");
+}
